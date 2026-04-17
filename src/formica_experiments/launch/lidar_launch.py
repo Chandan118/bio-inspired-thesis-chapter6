89b0ed82f214490d1ac8da4d33dd3db65e732152
Chandan Sheikder <40247073+Chandan118@users.noreply.github.com>
2026-04-17 10:18:15 +0800

    Add src/formica_experiments/launch/lidar_launch.py

---
+++ src/formica_experiments/launch/lidar_launch.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    channel_type = LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration(
+        'serial_port',
+        default='/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0'
+    )
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    
+    return LaunchDescription([
+        DeclareLaunchArgument('channel_type', default_value=channel_type),
+        DeclareLaunchArgument('serial_port', default_value=serial_port),
+        DeclareLaunchArgument('serial_baudrate', default_value=serial_baudrate),
+        DeclareLaunchArgument('frame_id', default_value=frame_id),
+        DeclareLaunchArgument('inverted', default_value=inverted),
+        DeclareLaunchArgument('angle_compensate', default_value=angle_compensate),
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_node',
+            name='rplidar_node',
+            parameters=[{
+                'channel_type': channel_type,
+                'serial_port': serial_port,
+                'serial_baudrate': serial_baudrate,
+                'frame_id': frame_id,
+                'inverted': inverted,
+                'angle_compensate': angle_compensate,
+                'auto_reconnect': True
+            }],
+            output='screen'
+        )
+    ])
